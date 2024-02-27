b661c45bb7373cb52aeb949de9c46cd50fe93dc7
mhussein00 <mohannad.hussein06@gmail.com>
2024-02-27 14:10:22 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ContactInfomation contact1 = new ContactInfomation("baka", "baka@bakamail.com");
+        BusinessContact business1 = new BusinessContact("eren", "erenyeager@aot.com", "614-614-614");
+        contact1.display();
+        business1.display();
+
+    }
+}
